19331b8d8a30d5b9a8d5e46f53c089c4897e4066
Jakub Adamski <jakub98adamski@gmail.com>
2021-12-01 21:54:59 +0000

    Loaded data to the dashboard. Fancier colors.

--- app.py
+++ app.py
@@ -1,43 +1,62 @@
 import dash
 from dash import dcc
 from dash import html
+import pandas as pd
+import plotly.graph_objs as go
 
 # For now copy example code from 
 # https://www.datacamp.com/community/tutorials/learn-build-dash-python
 
 app = dash.Dash()
 
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#FFFFF0', # ivory
+    'text': '#80461B',       # russet
+    'line': '#00A86B'        # jade
 }
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Hello Dash',
+        children='Hello Dash with a twist',
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
     ),
-    html.Div(children='Dash: A web application framework for Python.', style={
+    html.Div(children='Dash: A pretty visualisation tool for virtual CVs.', style={
         'textAlign': 'center',
         'color': colors['text']
     }),
     dcc.Graph(
-        id='Graph1',
+        id='life-exp-vs-gdp',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.8,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': colors['line']}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
             ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
         }
     )
 ])
