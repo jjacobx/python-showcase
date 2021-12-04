ba4fcc30a88e079e05604001f617cf5707d63b4c
Jakub Adamski <jakub98adamski@gmail.com>
2021-12-04 16:45:39 +0000

    Added more images and a CSS file

--- apps/super_resolution.py
+++ apps/super_resolution.py
@@ -5,6 +5,27 @@ from dash.dependencies import Input, Output
 from app import app
 
 layout = html.Div([
-    html.H3('Super-resolution of astronomical photos'), 
+    html.H1('Super-resolution of astronomical photos'), 
+    html.Div([
+        html.Div(
+            html.Figure([
+                html.P([
+                    html.Figcaption("DnCNN results"), 
+                    html.Img(src=app.get_asset_url('astro-dncnn.png'))
+                ], className='aligncenter')
+            ]), 
+            className = 'column'
+        ), 
+        html.Div(
+            html.Figure([
+                html.P([
+                    html.Figcaption("CycleGAN results"), 
+                    html.Img(src=app.get_asset_url('astro-cgan.png'))
+                ], className='aligncenter')
+            ]), 
+            className = 'column'
+        )
+    ], className='row'), 
+    html.Br(), 
     dcc.Link('Index', href='/index')
 ])
--- apps/terra_nova.py
+++ apps/terra_nova.py
@@ -5,6 +5,8 @@ from dash.dependencies import Input, Output
 from app import app
 
 layout = html.Div([
-    html.H3('terraNova hackathon'), 
+    html.H1('terra_Nova Hackathon'), 
+    html.Img(src=app.get_asset_url('australia.gif')), 
+    html.Br(), 
     dcc.Link('Index', href='/index')
 ])
